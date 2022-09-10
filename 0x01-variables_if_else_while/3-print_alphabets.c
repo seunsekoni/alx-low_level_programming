47e38e49c875d12575a80a2301c0237a98b6a368
seunsekoni <sekonioluwaseun@gmail.com>
2022-09-10 05:43:20 -0700

    lower upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print lowercase and then uppercase.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lowerupper;
+
+	for (lowerupper = 'a'; lowerupper <= 'z'; lowerupper++;)
+		putchar(lowerupper);
+	for(lowerupper = 'A'; lowerupper <= 'Z'; lowerupper++;)
+		putchar(lowerupper);
+	putchar('\n');
+
+	return (0);
+}
