d9eafdd734b59c32e4916ef83c5972ec9c73d474
seunsekoni <sekonioluwaseun@gmail.com>
2022-09-10 04:56:53 -0700

    LAst digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+ /**
+  * main - Entry point.
+  *
+  * Description: print random number and append
+  * 'Last digit of ' in its result
+  *
+  *  Return: Always 0.
+  */
+int main(void)
+{
+	int n;
+	char s[] = "Last digit of";
+	int l;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	l = n % 10;
+
+	if (l > 5)
+		printf("%s %d is %d and is greater than 5\n", s, n, l);
+	else if (l == 0)
+		printf("%s %d is %d and is 0\n", s, n, l);
+	else
+		printf("%s %d is %d and is less than 6 and not 0\n", s, n, l);
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+If else training and cruise.
