c554021c81064db3fccfd841c3274b716447de44
seunsekoni <sekonioluwaseun@gmail.com>
2022-09-10 06:05:42 -0700

    print using put char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print 0number zero to nine
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+        int val;
+
+        for (val = 0; val < 10; val++)
+                putchar(val);
+        putchar('\n');
+
+        return (0);
+}
