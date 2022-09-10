2be0db4cd71960c22880dc89d47ffa6ab3815214
seunsekoni <sekonioluwaseun@gmail.com>
2022-09-10 06:48:06 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the program
+ *
+ * Description: preinf hexadecimals
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int val;
+	char letter;
+
+	for (val = 0; val <= 9; val++)
+		putchar(val % 10 + '0');
+	for (letter = 'a'; letter <= 'f'; letter++)
+	       putchar(letter)
+	putchar('\n');
+
+	return (0);	
+}
