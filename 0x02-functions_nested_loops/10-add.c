9d914fb74fa7aa51e0133922d430d1f9bdccf998
seunsekoni <sekonioluwaseun@gmail.com>
2022-09-14 12:02:45 -0700

    Sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ *
+ * @a: number one
+ * @b: number two
+ *
+ * Return: Always 0 for success
+ */
+int add(int a, int b)
+{
+
+	return (a + b);
+}
